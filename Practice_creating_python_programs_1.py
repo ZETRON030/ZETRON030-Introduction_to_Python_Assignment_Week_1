34257c63d2206136491391c69b19e000ed750ff1
ZETRON030 <ikechukwulivingspring22@gmail.com>
2025-03-30 18:10:27 +0100

    Add files via upload
    Practice creating Python programs to perform arithmetic and string, experimenting with variables, and exploring data types.

---
+++ Practice_creating_python_programs_1.py
@@ -0,0 +1,39 @@
+#num = 89
+#print(num)
+#country = "Nigeria"
+#print(country)
+#License_Name = "MIT_license"
+#print(License_Name)
+ # Assigning new value to License_Name
+#License_Name = "GUI_license 😊"
+#print(License_Name)
+ 
+ #Assigning multiple values to multiple variables
+#e,f,g = 36, 5.4,"Welcome to AI"
+#print(e) 
+#print(f) 
+#print(g) 
+
+#List Data Type
+#Modules = ["Database", "Dart", "Web", "Fluter"]
+#print(Modules)
+
+#To Access List Items Above
+#print(Modules[1])
+#print(Modules[0])
+#print(Modules[3])
+
+#Dictionary Data Type
+#Capital_of_West_African_countries = {"Nigeria": "Abuja", "Ghana": "Accra", "Togo":"Lome"}
+#print(Capital_of_West_African_countries)
+
+#Arithmetic Operators In Python
+#add = 7 + 5
+#print(add)
+#sub = 9 - 4
+#print(sub)
+#exp = 4 ** 2 
+#print(exp)
+#floor = 10 // 3 
+#print(floor)
+
